6c0d099df21f7f57a34947f5a677bb4aa00bf07f
Antoniy Chonkov <antoniy@web-teh.net>
2011-11-08 18:03:14 +0200

    Fix build problem.

--- src/net/antoniy/beacontest/BeaconTestActivity.java
+++ src/net/antoniy/beacontest/BeaconTestActivity.java
@@ -1,15 +1,14 @@
 package net.antoniy.beacontest;
 
 import net.antoniy.beacon.Beacon;
-import net.antoniy.beacon.BeaconException;
 import net.antoniy.beacon.BeaconParams;
-import net.antoniy.beacon.R;
+import net.antoniy.beacon.exception.BeaconException;
 import net.antoniy.beacon.impl.BeaconFactory;
 import android.app.Activity;
 import android.os.Bundle;
 
 public class BeaconTestActivity extends Activity {
-private Beacon beacon;
+	private Beacon beacon;
 	
     /** Called when the activity is first created. */
     @Override
