d3cc41ba95bf36aea7ad09a6fafffdc782765328
Antoniy Chonkov <antoniy@web-teh.net>
2011-11-09 18:10:23 +0200

    Add a button for executing getAllActiveDevices. Change the initial configuration to work with the new implementation of the beacon.

--- src/net/antoniy/beacontest/BeaconTestActivity.java
+++ src/net/antoniy/beacontest/BeaconTestActivity.java
@@ -1,13 +1,26 @@
 package net.antoniy.beacontest;
 
+import java.util.List;
+
 import net.antoniy.beacon.Beacon;
+import net.antoniy.beacon.BeaconDeviceEventListener;
+import net.antoniy.beacon.BeaconEventListener;
 import net.antoniy.beacon.BeaconParams;
+import net.antoniy.beacon.DeviceInfo;
 import net.antoniy.beacon.exception.BeaconException;
+import net.antoniy.beacon.exception.BeaconStoppedException;
+import net.antoniy.beacon.exception.InvalidBeaconParamsException;
 import net.antoniy.beacon.impl.BeaconFactory;
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class BeaconTestActivity extends Activity {
+public class BeaconTestActivity extends Activity implements BeaconDeviceEventListener, BeaconEventListener {
+	
+	private static final String TAG = BeaconTestActivity.class.getSimpleName();
+	
 	private Beacon beacon;
 	
     /** Called when the activity is first created. */
@@ -26,12 +39,40 @@ public class BeaconTestActivity extends Activity {
         beaconParams.setDataClazz(BroadcastData.class);
         beaconParams.setSendInterval(1000);
         beaconParams.setUdpPort(10010);
+        beaconParams.setBeaconTimeout(30000);
+        beaconParams.setDataMaxSize(512);
         
         try {
 			beacon = BeaconFactory.createBeacon(this, beaconParams);
 		} catch (BeaconException e) {
 			e.printStackTrace();
+		} catch (InvalidBeaconParamsException e) {
+			e.printStackTrace();
 		}
+        
+        beacon.addBeaconDeviceEventListener(this);
+        beacon.addBeaconEventListener(this);
+        
+        Button getDiscoveredDevicesButton = (Button) findViewById(R.id.get_discovered_devices);
+        getDiscoveredDevicesButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				List<DeviceInfo> devices;
+				try {
+					devices = beacon.getActiveDevices();
+				} catch (BeaconStoppedException e) {
+					Log.w(TAG, e);
+					return;
+				}
+				
+				Log.i(TAG, "List all active devices:");
+				for (DeviceInfo deviceInfo : devices) {
+					Log.i(TAG, "DeviceInfo: " + deviceInfo.getHash());
+				}
+			}
+        	
+        });
     }
     
     @Override
@@ -46,10 +87,30 @@ public class BeaconTestActivity extends Activity {
     	try {
     		beacon.startBeacon();
 	    } catch (BeaconException e) {
-	    	e.printStackTrace();
+	    	Log.e(TAG, "Can't start beacon." , e);
 	    }
     	
     	super.onResume();
     }
+
+	@Override
+	public void discoveredBeaconDevice(DeviceInfo deviceInfo) {
+		Log.i(TAG, "New device: " + deviceInfo.getHash());
+	}
+
+	@Override
+	public void updateBeaconDevice(DeviceInfo deviceInfo) {
+		Log.i(TAG, "Update device: " + deviceInfo.getHash());
+	}
+
+	@Override
+	public void removeBeaconDevice(DeviceInfo deviceInfo) {
+		Log.i(TAG, "Remove device: " + deviceInfo.getHash());
+	}
+
+	@Override
+	public void beaconStopped() {
+		Log.i(TAG, "Beacon event stopped!");
+	}
     
 }
